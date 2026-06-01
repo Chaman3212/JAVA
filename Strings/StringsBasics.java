10f2bd612b0ccdce831763fdfd1e5e49961e91a9
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-06-01 23:04:17 +0530

    commit

---
+++ Strings/StringsBasics.java
@@ -0,0 +1,17 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class StringsBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        char[] arr = new char[10];
+//        String s = "chirag";
+//        System.out.println(s);
+
+        //taking input in stirng
+
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
