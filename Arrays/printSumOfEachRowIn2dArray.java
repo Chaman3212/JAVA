2d720a0ce3ae899fd4c353cfe9b1ce9448b0bc7e
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-06-28 23:36:49 +0530

    commit

--- Arrays/printSumOfEachRowIn2dArray.java
+++ Arrays/printSumOfEachRowIn2dArray.java
@@ -4,24 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class printSumOfEachRowIn2dArray {
-    public static ArrayList<Integer> sumOfEachRow(int[][] nums ){
+    public static ArrayList<Integer> printSumOfEachRow(int[][] nums){
         ArrayList<Integer> list = new ArrayList<>();
-       for(int j=0;j<nums[0].length;j++){
-           int sum =0;
-           for (int i = 0; i < nums.length; i++) {
-               sum +=nums[i][j];
-           }
-
-
-
-
-           list.add(sum);
-       }
+        for (int i = 0; i < nums.length; i++) {
+            int sum =0;
+            for (int j = 0; j < nums[i].length; j++) {
+                sum+=nums[i][j];
+            }
+            list.add(sum);
+        }
         return list;
     }
 
     static void main(String[] args) {
         int[][] nums = {{1,2,3},{4,5,6},{7,8,9}};
-        System.out.println(sumOfEachRow(nums));
+        System.out.println(printSumOfEachRow(nums));
     }
 }
