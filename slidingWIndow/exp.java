e033679663e22498f85b81c8253b17b476ab2daf
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-07-25 22:24:57 +0530

    commit

---
+++ slidingWIndow/exp.java
@@ -0,0 +1,15 @@
+package slidingWIndow;
+
+public class exp {
+    public static void maxOfSubarray(int [] nums , int k){
+        int a=nums[0], b, c;
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+    static void main(String[] args) {
+        int[] nums = {2,3,55,12,44,88,76,33};
+
+    }
+
+}
